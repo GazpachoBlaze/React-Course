cd0b655d6c558a1e74fcf87d1d7e2c8805c9e5db
Gazpacho <Gazpachoblaze@gmail.com>
2024-06-12 14:43:06 -0500

    1st commit

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+function header() {
+    return(
+        <header>
+            <h1>My Website</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Contract</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
