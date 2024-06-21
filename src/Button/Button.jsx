2a531297fd5a6d02aa8d349dd8f76901a0e31485
Gazpacho <Gazpachoblaze@gmail.com>
2024-06-21 16:45:50 -0500

    created button folder jsx css / inline

---
+++ src/Button/Button.jsx
@@ -0,0 +1,7 @@
+import bob from "./Button.module.css"
+
+function Button() {
+    return (<button className={bob.button}>click me</button>);
+}
+
+export default Button
--- src/Food.jsx
+++ src/Food.jsx
@@ -1,9 +1,14 @@
 function Food() {
 
+    const styles = {
+        color: "green",
+
+    }
+
     const food1 = "Orange";
     const food2 = "Mango";
     return (
-        <ul>
+        <ul style={styles}>
             <li>Apple</li>
             <li>{food2}</li>
             <li>{food1.toUpperCase()}</li>
