376cba2e027559e2c2c43a80373b9ea3b6cbeffb
Gazpacho <Gazpachoblaze@gmail.com>
2024-06-21 17:34:42 -0500

    importing props

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx';
 import Button from './Button/Button.jsx';
+import Student from './Student.jsx';
 
 function App() {
   return (
     <>
       <Header/>
       <Card/>
+      <Student name="Spongebob" age={38}/>
       <Button/>
       <Food/>
       <Footer/>
