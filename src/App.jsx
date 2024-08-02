310f1e2f855ad8d39f4fcd685492e7f5fc8b50e5
Gazpacho <Gazpachoblaze@gmail.com>
2024-08-02 18:03:52 -0500

    changed array and list component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,25 @@ import Student from './Student.jsx'
 import UserGreeting from './UserGreeting.jsx'
 
 function App() {
+
+  const fruits = [
+    {id: 1, name: "Mango", cost: 3.00},
+    {id: 2, name: "Peach", cost: 1.50},
+    {id: 3, name: "Pineapple", cost: 6.00},
+    {id: 4, name: "Grape", cost: 4.00},
+];
+
+// fruits.sort((a, b) => a.name.localeCompare(b.name)); //alphabetical
+    // //reverse alphabetical = fruits.sort((a, b) => b.name.localeCompare(a.name));  
+    // fruits.sort((a, b) => a.cost -b.cost); 
+
+
+    // const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp; <b>{fruit.cost}</b></li>);
+    // return (<ul>{listItems}</ul>);
+
+    
   return (
+    
     <>
       <Header/>
       <Card/>
@@ -21,6 +39,7 @@ function App() {
       <Button/>
       <Food/>
       <List/>
+      <List items={fruits} category="Fruits"/>;
       <Footer/>
     </>
   );
